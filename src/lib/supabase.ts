fdb9ae69c492ab8fbced99a9dfbde54d0f0e58dc
Annie Sexton <anniesexton@Annies-Laptop.local>
2024-04-18 13:10:32 -0700

    print env vars

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 import type { Database } from "../../types/supabase";
 
+console.log(import.meta.env.SUPABASE_URL);
+console.log(import.meta.env.SUPABASE_ANON_KEY);
+
 export const supabase = createClient(
   import.meta.env.SUPABASE_URL,
   import.meta.env.SUPABASE_ANON_KEY,
