f830473a930d817d81439edf795348659b8103ed
Annie Sexton <73925141+anniebabannie@users.noreply.github.com>
2024-04-18 15:59:09 -0700

    added migrations for styles (#1)
    * added migrations for styles
    
    * added review workflow
    
    * added stuff
    
    * typo in gh action:
    
    * print env vars
    
    * print env vars
    
    * print env vars
    
    * added migration
    
    * wip
    
    * update gh env
    
    * cleanup env
    
    * WIP
    
    ---------
    
    Co-authored-by: Annie Sexton <anniesexton@Annies-Laptop.local>

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,8 +2,8 @@ import { createClient } from "@supabase/supabase-js";
 import type { Database } from "../../types/supabase";
 
 export const supabase = createClient(
-  import.meta.env.SUPABASE_URL,
-  import.meta.env.SUPABASE_ANON_KEY,
+  process.env.SUPABASE_URL as string,
+  process.env.SUPABASE_ANON_KEY as string,
   {
     auth: {
       flowType: "pkce",
---
+++ supabase/migrations/20240418221849_remove_image_from_styles.sql
@@ -0,0 +1,1 @@
+alter table "public"."styles" drop column "image";
