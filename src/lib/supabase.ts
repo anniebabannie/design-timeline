c45c6daf0b3a27b4dda15f4072a4ff207e4d6e18
Annie Sexton <anniesexton@Annies-Laptop.local>
2024-04-18 15:14:44 -0700

    added migration

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,9 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 import type { Database } from "../../types/supabase";
 
-console.log("process.env.SUPABASE_URL", process.env.SUPABASE_URL);
-console.log("process.env.SUPABASE_ANON_KEY", process.env.SUPABASE_ANON_KEY);
-
 export const supabase = createClient(
   process.env.SUPABASE_URL as string,
   process.env.SUPABASE_ANON_KEY as string,
